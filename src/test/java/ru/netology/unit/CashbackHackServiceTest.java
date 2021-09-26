384bfcf4a64f33de7ce71c75d0a8af1d18b17d8d
Larisa Avtukhova <aln1967@yandex.ru>
2021-09-26 15:14:46 +0300

    Changes test

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -30,7 +30,7 @@ class CashbackHackServiceTest {
         int amount = 1000;
         int expected = 0;
         int actual = cashbackHackService.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(actual, expected, "Сумма докупки до начисления бонуса: 0 рублей");
     }
 
     @Test
